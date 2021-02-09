73779b38e7d5073d5c47b5c0f775f23df788e20f
Julio Cesar GCordeiro.EXT <JulioCesar.GCordeiro.EXT@thomsonreuters.com>
2021-02-09 13:41:57 -0300

    Primeiro Commit

---
+++ src/views/DesignList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="designList">
+    <h1>Meus Templates</h1>
+
+    <p>
+      <router-link to="/dashboard/new">Novo Template</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'designList',
+  components: {
+  },
+  methods: {
+  }
+}
+</script>
